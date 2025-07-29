d2cdeba1e889dcdc59d60fc11066bda6662c9cb8
SammL1 <36447856+Samml1@users.noreply.github.com>
2025-07-29 17:02:32 +0100

    Update yml_writer.py

--- yml_writer.py
+++ yml_writer.py
@@ -5,9 +5,9 @@ from classStructure import player, room
 user1 = player("TestName", "entrance")
 
 # Create the dungeon rooms
-entrance = room('Dungeon Entrance', 'The Entrance to the dungeon')
-hallway = room('Dungeon hallway', 'A dark wet hallway that smells of mildew')
-treasureRoom = room('Treasure Room', 'A room full of a horde of treasure')
+entrance = room('entrance','Dungeon Entrance', 'The Entrance to the dungeon')
+hallway = room('hallway','Dungeon hallway', 'A dark wet hallway that smells of mildew')
+treasureRoom = room('treasureRoom','Treasure Room', 'A room full of a horde of treasure')
 
 # Create exits
 entrance.exits["north"] = hallway
@@ -22,9 +22,19 @@ treasureRoom.enemyRoom = True
 
 roomList = [entrance, hallway, treasureRoom]
 
-# Write YAML file
-with io.open('player.yaml', 'w', encoding='utf8') as outfile:
-    yaml.dump(user1, outfile, default_flow_style=False, allow_unicode=True)
+# strip class data to present for yaml file
+def tokeniseClass(objectList):
+    for i in range(len(objectList)):
+        print(roomList[i].id)
+        print(roomList[i].roomName)
+        print(roomList[i].description)
+        exitid=str(roomList[i].exits.values()).lstrip("dict_values([").rstrip("])")
+        print(exitid)
+        print(str(roomList[i].enemyRoom) + "\n")
+
+
+tokeniseClass(roomList)
+
 
 with io.open('rooms.yaml', 'w', encoding='utf8') as outfile:
     yaml.dump(roomList[0], outfile, explicit_start=True, default_flow_style=False, allow_unicode=True)
